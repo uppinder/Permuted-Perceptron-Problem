d63b6d643b9b0fde79f34270c4a656489a370fea
Uppinder <uppinder@iitg.ernet.in>
2016-05-26 20:49:07 +0800

    Initialize random search for PPP in C.

---
+++ randomSearchPPP.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc,char* argv[]) {
+
+	/*
+		Take random e-matrix A(mxn) and a non-negative
+		vector S(mx1). Generate random e-vectors X(nx1).
+		Check if AX = S. If yes, report X else change one 
+		element of X.
+
+	 */
+
+	return 0;
+}
